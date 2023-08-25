72cf3c58d0621094558f83110b8428884c7f79e1
Andrew <andrewkoval2003@gmail.com>
2023-08-26 00:11:22 +0100

    Organized items by categories

--- src/components/category-form.jsx
+++ src/components/category-form.jsx
@@ -1,12 +1,81 @@
 import React, { useState, useEffect } from "react";
+const fs = require('fs');
+
 
 export default function CategoryForm(props){
 
+    
+    function fetchItems() {
+        return fetch("http://192.168.0.174:3004/items/").then((response) => {
+            if (!response.ok) {
+                throw new Error("Unexpected Server response");
+            }
+            return response.json();
+        });
+    }
+    const [item, setItem] = useState([]);
+    const [category, setCategory] = useState([]);
+
+    useEffect(() => {
+        fetchItems().then((data) => setItem(data));
+      }, []);
+    
+
+    // function getListOfCategories(items){
+    //     var categories = [];
+    //     for(const item of items){
+    //       if(!categories.includes(item.category)){
+    //         categories.push(item.category);
+    //       } 
+    //     }
+    //     console.log(categories);
+    //     //send categories to server
+    //     fetch('http://localhost:3000/categories', {
+    //         method: 'POST',
+    //         headers: {
+    //           'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(categories)
+    //       })
+    //       .then(response => {
+    //         if(!response.ok) {
+    //           throw new Error('Network response was not ok');
+    //         }
+    //         console.log('Categories sent to server');
+    //       })
+    //       .catch(error => {
+    //         console.error('Error sending categories to server:', error);
+    //       });
+    //   }
+
+
+    // function displayCategories() {
+    //     fetchItems()
+    //       .then((items) => {
+    //         getListOfCategories(items); })
+    //       .catch((error) => console.log("Error: ", error));
+    //   }
+
+    
 
     return(
         <div>
+          <form type="submit">
+            <table>
+                <thead> 
+                  <td>name</td>
+                </thead>
+                <tbody>
+                  <td></td>
+                </tbody>
+            </table>
+
+          </form>
+
+
+        <button className="btn btn-primary" onClick={displayCategories}> get list</button>
             <form action="">
-                
+                   
             <div className="row">
                     <div className="col-sm-4 d-grid">
                         <button onClick={() => props.showList()} type="button" className="btn btn-secondary me-2" > Cancel</button>
--- src/components/items.jsx
+++ src/components/items.jsx
@@ -1,17 +1,37 @@
 import React, {useEffect, useState} from "react";
 import Barcode from 'react-barcode';
+import ItemForm from "./item-form";
+import CategoryForm from "./category-form";
+
 import Dashboard from "./dashboard";
 
+// var IpAddress = "localhost";
+var IpAddress = "192.168.0.174";
 
 export function Items(){
-    const [content, setContent] = useState(<ItemList showForm={showForm} />);
+    const [content, setContent] = useState(
+    <>  
+        <Dashboard showForm={showForm} showCategoryForm={showCategoryForm}/>
+        <ItemList showForm={showForm} />
+    </>
+    );
 
     function showList(){
-        setContent(<ItemList showForm={showForm}/>);
+        setContent(
+            <>
+              <Dashboard showForm={showForm} showCategoryForm={showCategoryForm}/>
+              <ItemList showForm={showForm}/>
+            </>
+        
+        );
     }
     
     function showForm(item){
         setContent(<ItemForm item={item} showList={showList} />);
+    }
+
+    function showCategoryForm(){
+        setContent(<CategoryForm showList={showList}/>)
     }
     
     return(
@@ -28,7 +48,7 @@ function ItemList(props){
     const [item, setItem] = useState([]);
 
     function fetchItems(){
-        fetch ('http://192.168.0.174:3004/items')
+        fetch ('http://' + IpAddress +':3004/items')
         .then(response => {
             if(!response.ok)
                 throw new Error("Unexpected Server response")
@@ -42,24 +62,24 @@ function ItemList(props){
     }
     useEffect(() => fetchItems(), []);
     function deleteItem(id){
-        fetch("http://192.168.0.174:3004/items/" + id, {method: "DELETE"})
+        fetch('http://' + IpAddress +':3004/items' + id, {method: "DELETE"})
         .then((response) => response.json())
         .then((data) => fetchItems())
     }
 
     return(
+
         <div className="container my-5">
 
                 {/* <Dashboard/> */}
 
-            <h2 className="text-center mb-3"> List of Items</h2>
-            <button onClick={() => props.showForm({})} type="button" className="btn btn-primary me-2" > Create</button>
-            <button onClick={() => fetchItems()} type="button" className="btn btn-primary me-2" > Refresh</button>
+            {/* <h2 className="text-center mb-3"> List of Items</h2> */}
             <table className="table">
                 <thead>
 
                     <tr>
                         <th>Item Name</th>
+                        <th>Brand</th>
                         <th>Barcode</th>
                         <th>Item Quantity</th>
                         <th>Item Price</th>
@@ -72,7 +92,8 @@ function ItemList(props){
                             return(
                                 <tr key={index}>
                                     <td>{item.name}</td>
-                                    <td id="barcodeTd"><Barcode value={item.barcode} className="barcode" /></td>
+                                    <td>{item.brand}</td>
+                                    <td id="barcodeTd"><Barcode height={30 + 'px'} width={1 + 'px'} fontSize={14 + 'pt'} value={item.barcode} className="barcode" /></td>
                                     <td>{item.quantity}</td>
                                     <td>€{item.price}</td>
                                     <td >
@@ -89,139 +110,139 @@ function ItemList(props){
     )
 }
 
-function ItemForm(props){
-    const [errorMessage, setErrorMessage] = useState("")
+// function ItemForm(props){
+//     const [errorMessage, setErrorMessage] = useState("")
 
-    function handleSubmit(event){
-        event.preventDefault();
+//     function handleSubmit(event){
+//         event.preventDefault();
 
-        //read form data
-        const formData = new FormData(event.target);
+//         //read form data
+//         const formData = new FormData(event.target);
 
-        //convert form data to json object
-        const item = Object.fromEntries(formData.entries());
+//         //convert form data to json object
+//         const item = Object.fromEntries(formData.entries());
 
-        //form validation
-        if(!item.name || !item.barcode || !item.quantity || !item.category || !item.image){
+//         //form validation
+//         if(!item.name || !item.barcode || !item.quantity || !item.category || !item.image){
 
-            console.log("please fill out all fields")
-            setErrorMessage(
-                <div className="alert alert-warning" role="alert">
-                    Please fill out all fields.
-                </div>);
-            return;
-        }
+//             console.log("please fill out all fields")
+//             setErrorMessage(
+//                 <div className="alert alert-warning" role="alert">
+//                     Please fill out all fields.
+//                 </div>);
+//             return;
+//         }
 
 
-        if(props.item.id){
-            //update
-            fetch("http://192.168.0.174:3004/items/" + props.item.id, {
-                method: "PATCH",
-                headers:{
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(item)
-            })
-            .then((response) => {
-                if(!response.ok){
-                    throw new Error("Unexpected Server Response");
-                } return response.json()
-            })
-            .then((data) => props.showList())
-            .catch((error) => {
-                console.log("Error: ", error);
-            })
+//         if(props.item.id){
+//             //update
+//             fetch('http://' + IpAddress +':3004/items' + props.item.id, {
+//                 method: "PATCH",
+//                 headers:{
+//                     "Content-Type": "application/json"
+//                 },
+//                 body: JSON.stringify(item)
+//             })
+//             .then((response) => {
+//                 if(!response.ok){
+//                     throw new Error("Unexpected Server Response");
+//                 } return response.json()
+//             })
+//             .then((data) => props.showList())
+//             .catch((error) => {
+//                 console.log("Error: ", error);
+//             })
 
-        }
-        else{
-        //create new item
-            fetch("http://192.168.0.174:3004/items", {
-                method: "POST",
-                headers:{
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(item)
-            })
-            .then((response) => {
-                if(!response.ok){
-                    throw new Error("Unexpected Server Response");
-                } return response.json()
-            })
-            .then((data) => props.showList())
-            .catch((error) => {
-                console.log("Error: ", error);
-            })
-    }
-}
-    return(
-        <div className="container my-5">
-            <h2 className="text-center mb-3"> {props.item.id ? "Edit Item" : "Create New Item"}</h2>
+//         }
+//         else{
+//         //create new item
+//             fetch('http://' + IpAddress +':3004/items', {
+//                 method: "POST",
+//                 headers:{
+//                     "Content-Type": "application/json"
+//                 },
+//                 body: JSON.stringify(item)
+//             })
+//             .then((response) => {
+//                 if(!response.ok){
+//                     throw new Error("Unexpected Server Response");
+//                 } return response.json()
+//             })
+//             .then((data) => props.showList())
+//             .catch((error) => {
+//                 console.log("Error: ", error);
+//             })
+//     }
+// }
+//     return(
+//         <div className="container my-5">
+//             <h2 className="text-center mb-3"> {props.item.id ? "Edit Item" : "Create New Item"}</h2>
 
-            <div className="row">
-                <div className="col-lg-6 mx-auto">
-                    {errorMessage}
-                    <form onSubmit={(event) => handleSubmit(event)}>
-                        <div className="row mb-3">
-                            <label className="col-sm-4 col-form-label">Name</label>
-                            <div className="col-sm-8">
-                                <input type="text" className="form-control" name="name" defaultValue={props.item.name} />
-                            </div>
-                        </div>
+//             <div className="row">
+//                 <div className="col-lg-6 mx-auto">
+//                     {errorMessage}
+//                     <form onSubmit={(event) => handleSubmit(event)}>
+//                         <div className="row mb-3">
+//                             <label className="col-sm-4 col-form-label">Name</label>
+//                             <div className="col-sm-8">
+//                                 <input type="text" className="form-control" name="name" defaultValue={props.item.name} />
+//                             </div>
+//                         </div>
 
-                        <div className="row mb-3">
-                            <label className="col-sm-4 col-form-label">Barcode</label>
-                            <div className="col-sm-8">
-                                <input type="text" className="form-control" name="barcode" defaultValue={props.item.barcode} />
-                            </div>
-                        </div>
+//                         <div className="row mb-3">
+//                             <label className="col-sm-4 col-form-label">Barcode</label>
+//                             <div className="col-sm-8">
+//                                 <input type="text" className="form-control" name="barcode" defaultValue={props.item.barcode} />
+//                             </div>
+//                         </div>
 
-                        <div className="row mb-3">
-                            <label className="col-sm-4 col-form-label">Quantity</label>
-                            <div className="col-sm-8">
-                                <input type="text" className="form-control" name="quantity" defaultValue={props.item.quantity} />
-                            </div>
-                        </div>
+//                         <div className="row mb-3">
+//                             <label className="col-sm-4 col-form-label">Quantity</label>
+//                             <div className="col-sm-8">
+//                                 <input type="text" className="form-control" name="quantity" defaultValue={props.item.quantity} />
+//                             </div>
+//                         </div>
 
-                        <div className="row mb-3">
-                            <label className="col-sm-4 col-form-label">Category</label>
-                            <div className="col-sm-8">
-                                <select type="text"
-                                    className="form-select"
-                                    name="category"
-                                    defaultValue={props.item.category}>
-                                    <option value="Bolts"> Bolts</option>
-                                    <option value="Nuts"> Nuts</option>
-                                    <option value="Sheets"> Sheets</option>
-                                    <option value="Screws"> Screws</option>
-                                    <option value="Misc"> Misc</option>
-                                </select>
-                            </div>
-                        </div>                
-                        <div className="row mb-3">
-                            <label className="col-sm-4 col-form-label">Image Url</label>
-                            <div className="col-sm-8">
-                                <input type="text" className="form-control" name="image" defaultValue={props.item.image} />
-                            </div>
-                        </div>
-                        {props.item.id && <div className="row mb-3">
-                            <label className="col-sm-4 col-form-label">ID</label>
-                            <div className="col-sm-8">
-                                <input readOnly className="form-control-plaintext" name="id" defaultValue={props.item.id} />
-                            </div>
-                        </div>}
+//                         <div className="row mb-3">
+//                             <label className="col-sm-4 col-form-label">Category</label>
+//                             <div className="col-sm-8">
+//                                 <select type="text"
+//                                     className="form-select"
+//                                     name="category"
+//                                     defaultValue={props.item.category}>
+//                                     <option value="Bolts"> Bolts</option>
+//                                     <option value="Nuts"> Nuts</option>
+//                                     <option value="Sheets"> Sheets</option>
+//                                     <option value="Screws"> Screws</option>
+//                                     <option value="Misc"> Misc</option>
+//                                 </select>
+//                             </div>
+//                         </div>                
+//                         <div className="row mb-3">
+//                             <label className="col-sm-4 col-form-label">Image Url</label>
+//                             <div className="col-sm-8">
+//                                 <input type="text" className="form-control" name="image" defaultValue={props.item.image} />
+//                             </div>
+//                         </div>
+//                         {props.item.id && <div className="row mb-3">
+//                             <label className="col-sm-4 col-form-label">ID</label>
+//                             <div className="col-sm-8">
+//                                 <input readOnly className="form-control-plaintext" name="id" defaultValue={props.item.id} />
+//                             </div>
+//                         </div>}
 
-                        <div className="row">
-                            <div className="col-sm-4 d-grid">
-                                <button onClick={() => props.showList()} type="button" className="btn btn-secondary me-2" > Cancel</button>
-                            </div>
-                            <div className="col-sm-4 d-grid">
-                                <button type="submit" className="btn btn-primary me-2"> Save</button>
-                            </div>
-                        </div>
+//                         <div className="row">
+//                             <div className="col-sm-4 d-grid">
+//                                 <button onClick={() => props.showList()} type="button" className="btn btn-secondary me-2" > Cancel</button>
+//                             </div>
+//                             <div className="col-sm-4 d-grid">
+//                                 <button type="submit" className="btn btn-primary me-2"> Save</button>
+//                             </div>
+//                         </div>
                            
-                    </form>
-                </div>
-            </div>
-        </div>
-    )
-}
+//                     </form>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
